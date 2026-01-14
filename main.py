71f9ea3f47bf3799c85a6383047f2ee86be98219
denishdhanabalan1224 <denishdhanabalan1224@gmail.com>
2026-01-14 14:39:32 +0530

    Update main.py
    Add new API endpoint /messages/v1

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ def home():
 @app.get("/messages")
 def get_messages():
     return {"messages": ["Hello", "Welcome"]}
+@app.get("/messages/v1")
+def get_messages_v1():
+    return {
+        "messages": ["Hello", "Welcome"],
+        "version": "v1"
+    }
